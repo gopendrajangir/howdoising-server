a815fb24f066b27edc237c7aefcfdb557f3185b7
Gopendra <gopendra1.kumar@gmail.com>
2022-04-05 02:13:15 +0530

    some changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: ['https://gopendrajangir.github.io', 'http://localhost:3000'],
     credentials: true,
-    allowedHeaders: ['Authorization'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
   })
 );
 
