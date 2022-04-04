5215d0f55051554668c1020b83bf9d9314293513
Gopendra <gopendra1.kumar@gmail.com>
2022-04-04 12:58:56 +0530

    some changes

--- utils/email.js
+++ utils/email.js
@@ -22,7 +22,7 @@ module.exports = class Email {
       const transport = nodemailer.createTransport({
         service: 'SendinBlue',
         auth: {
-          user: process.env.SENDGRID_USERNAME,
+          user: process.env.SENDINBLUE_USERNAME,
           pass: process.env.SENDINBLUE_PASSWORD,
         },
       });
