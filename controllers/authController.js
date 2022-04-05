b0e1783aa09b9f00bf20de98aba36213eebf2bc2
Gopendra <gopendra1.kumar@gmail.com>
2022-04-05 13:42:23 +0530

    some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: false,
+    sameSite: 'none',
   });
 
   user.password = undefined;
